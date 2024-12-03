2546a55969e10c17efbcb58f4cad86113cc2c0d9
WalkUnderPressure <alexwsdaet@gmail.com>
2024-12-03 15:33:24 +0200

    Add ping api-point

--- src/index.ts
+++ src/index.ts
@@ -51,6 +51,10 @@ app.use(authMiddleware).use('/comments', commentsRouter);
 app.use(authMiddleware).use('/posts', postsRouter);
 app.use(authMiddleware).use('/notifications', notificationsRouter);
 
+app.get('/ping', (req: Request, res: Response) => {
+  return res.json({ "pong": 1 });
+})
+
 const PORT = process.env.PORT || 7000;
 
 async function startup() {
